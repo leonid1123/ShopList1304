f3a45c76471b9c927fe2408e96bea998dd494a81
Leo <47742726+leonid1123@users.noreply.github.com>
2022-02-15 12:09:40 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopList1304
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Name;
+            int Price;
+            int Quantity;
+            Name = textBox1.Text.Trim();
+            bool PriceOK = int.TryParse(textBox2.Text.Trim(), out Price);
+            bool QuantityOK = int.TryParse(textBox3.Text.Trim(),out Quantity);
+            if(PriceOK && QuantityOK&&Name.Length>0)
+            {
+                listBox1.Items.Add(Name + " " + Price.ToString()+"p." + Quantity.ToString() + "шт.");
+            }
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            //TODO не должно быть дубликатов записей
+        }
+
+
+    }
+}
