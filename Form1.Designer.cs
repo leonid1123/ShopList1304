598a5a91fe5a96603b6db3d19863a7787f5e65d6
Leo <47742726+leonid1123@users.noreply.github.com>
2022-03-29 12:17:56 +0300

    fix

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,6 +40,12 @@
             this.button3 = new System.Windows.Forms.Button();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
@@ -47,7 +53,7 @@
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Location = new System.Drawing.Point(12, 31);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(257, 344);
             this.listBox1.TabIndex = 0;
@@ -56,7 +62,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(275, 12);
+            this.button1.Location = new System.Drawing.Point(275, 31);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(291, 40);
             this.button1.TabIndex = 1;
@@ -67,7 +73,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(431, 58);
+            this.textBox1.Location = new System.Drawing.Point(431, 77);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(135, 26);
             this.textBox1.TabIndex = 2;
@@ -76,7 +82,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(275, 64);
+            this.label1.Location = new System.Drawing.Point(275, 83);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(91, 20);
             this.label1.TabIndex = 5;
@@ -86,7 +92,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(275, 120);
+            this.label2.Location = new System.Drawing.Point(275, 139);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(109, 20);
             this.label2.TabIndex = 6;
@@ -96,7 +102,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(275, 92);
+            this.label3.Location = new System.Drawing.Point(275, 111);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(52, 20);
             this.label3.TabIndex = 7;
@@ -106,7 +112,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(525, 89);
+            this.label4.Location = new System.Drawing.Point(525, 108);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 20);
             this.label4.TabIndex = 8;
@@ -116,7 +122,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(12, 371);
+            this.label5.Location = new System.Drawing.Point(12, 390);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(214, 25);
             this.label5.TabIndex = 9;
@@ -126,7 +132,7 @@
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button2.ForeColor = System.Drawing.Color.Red;
-            this.button2.Location = new System.Drawing.Point(275, 305);
+            this.button2.Location = new System.Drawing.Point(275, 324);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(291, 51);
             this.button2.TabIndex = 10;
@@ -138,7 +144,7 @@
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.button3.Location = new System.Drawing.Point(275, 154);
+            this.button3.Location = new System.Drawing.Point(279, 177);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(291, 48);
             this.button3.TabIndex = 11;
@@ -149,24 +155,69 @@
             // maskedTextBox1
             // 
             this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.maskedTextBox1.Location = new System.Drawing.Point(431, 92);
+            this.maskedTextBox1.Location = new System.Drawing.Point(431, 111);
+            this.maskedTextBox1.Mask = "999999999";
             this.maskedTextBox1.Name = "maskedTextBox1";
             this.maskedTextBox1.Size = new System.Drawing.Size(88, 27);
             this.maskedTextBox1.TabIndex = 13;
+            this.maskedTextBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.maskedTextBox1_MouseClick);
             // 
             // maskedTextBox2
             // 
             this.maskedTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.maskedTextBox2.Location = new System.Drawing.Point(431, 125);
+            this.maskedTextBox2.Location = new System.Drawing.Point(431, 144);
+            this.maskedTextBox2.Mask = "99999999999999";
             this.maskedTextBox2.Name = "maskedTextBox2";
             this.maskedTextBox2.Size = new System.Drawing.Size(135, 27);
             this.maskedTextBox2.TabIndex = 14;
+            this.maskedTextBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.maskedTextBox2_MouseClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(596, 28);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "Файл";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(59, 24);
+            this.toolStripMenuItem1.Text = "Файл";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(166, 26);
+            this.toolStripMenuItem2.Text = "Сохранить";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(166, 26);
+            this.toolStripMenuItem3.Text = "Открыть";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(118, 24);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 445);
+            this.ClientSize = new System.Drawing.Size(596, 440);
             this.Controls.Add(this.maskedTextBox2);
             this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.button3);
@@ -179,9 +230,13 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Список покупок";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,6 +256,11 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ShopList1304
 {
@@ -39,9 +40,9 @@ namespace ShopList1304
         private void button1_Click(object sender, EventArgs e)//Добавить
         {
             int tmpPrice;
-            bool priceOK = int.TryParse(textBox2.Text, out tmpPrice);
+            bool priceOK = int.TryParse(maskedTextBox1.Text.Trim(), out tmpPrice);
             int tmpQuantity;
-            bool quantityOK = int.TryParse(textBox3.Text, out tmpQuantity);
+            bool quantityOK = int.TryParse(maskedTextBox2.Text.Trim(), out tmpQuantity);
 
             if (textBox1.Text.Length > 0 && priceOK && quantityOK)
             {
@@ -49,8 +50,8 @@ namespace ShopList1304
                 foodList.Add(newFood);
                 ListPrint(foodList);
                 textBox1.Clear();
-                textBox2.Clear();
-                textBox3.Clear();
+                maskedTextBox1.Clear();
+                maskedTextBox2.Clear();
                 SumCount(foodList);
             }
         }
@@ -76,24 +77,24 @@ namespace ShopList1304
         void SumCount(List<Food> foodToSum)
         {
             int sum = 0;
-            foreach (Food food in foodToSum) 
-            { 
-                sum+=food.Price*food.Quantity;
+            foreach (Food food in foodToSum)
+            {
+                sum += food.Price * food.Quantity;
             }
-            label5.Text = "Итоговая стоимость: "+sum.ToString();
-            
+            label5.Text = "Итоговая стоимость: " + sum.ToString();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             textBox1.Text = foodList[listBox1.SelectedIndex].Name;
-            textBox2.Text = foodList[listBox1.SelectedIndex].Price.ToString();
-            textBox3.Text = foodList[listBox1.SelectedIndex].Quantity.ToString();
+            maskedTextBox1.Text = foodList[listBox1.SelectedIndex].Price.ToString();
+            maskedTextBox2.Text = foodList[listBox1.SelectedIndex].Quantity.ToString();
         }
 
         private void button3_Click(object sender, EventArgs e)//изменить позицию
@@ -101,9 +102,40 @@ namespace ShopList1304
             if (listBox1.SelectedIndex > -1)
             {
                 foodList[listBox1.SelectedIndex].Name = textBox1.Text;
-                foodList[listBox1.SelectedIndex].Price = textBox2.Text;
-                foodList[listBox1.SelectedIndex].Quantity = textBox3.Text;
+                foodList[listBox1.SelectedIndex].Price = int.Parse(maskedTextBox1.Text);
+                foodList[listBox1.SelectedIndex].Quantity = int.Parse(maskedTextBox2.Text);
+                ListPrint(foodList);
+                SumCount(foodList);
             }
+        }
+
+        private void maskedTextBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            maskedTextBox1.SelectAll();
+        }
+
+        private void maskedTextBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            maskedTextBox2.SelectAll();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.ShowDialog();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            foreach(Food food in foodList)
+            {
+                String tmp = food.Name+food.Price.ToString()+food.Quantity.ToString();
+                File.WriteAllText("WriteLines.txt",tmp);
+
+            }
+
+
+
         }
     }
 }
